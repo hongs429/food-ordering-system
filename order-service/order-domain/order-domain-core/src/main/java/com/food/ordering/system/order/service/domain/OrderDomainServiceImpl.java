5bcd2f6d37c0cbe6e68ce81d59d168a00f656cb0
hongs429 <honshyeon@gmail.com>
2025-12-03 08:01:01 +0900

    도메인 서비스 생성

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,80 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.domain.vo.ProductId;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.OrderItem;
+import com.food.ordering.system.order.service.domain.entity.Product;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.event.OrderCreateEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OrderDomainServiceImpl implements OrderDomainService {
+
+    private static final ZonedDateTime UTC_NOW_TIME = ZonedDateTime.now(ZoneId.of("UTC"));
+
+    @Override
+    public OrderCreateEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order, restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        log.info("Order with id: {} is initiated", order.getId());
+        return new OrderCreateEvent(order, UTC_NOW_TIME);
+    }
+
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+        Map<ProductId, Product> orderItemToMap = order.getItems().stream().collect(Collectors.toMap(
+                orderItem -> orderItem.getProduct().getId(), OrderItem::getProduct
+        ));
+
+        restaurant.getProducts().forEach(orderItem -> {
+            if (orderItemToMap.containsKey(orderItem.getId())) {
+                Product product = orderItemToMap.get(orderItem.getId());
+                product.updateWithConfirmedNameAndPrice(orderItem.getName(), orderItem.getPrice());
+            }
+        });
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+        if (!restaurant.isActive()) {
+            throw new OrderDomainException("Restaurant is not active. restaurant id: " + restaurant.getId());
+        }
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        order.pay();
+        log.info("Order with id: {} has been paid", order.getId());
+        return new OrderPaidEvent(order, UTC_NOW_TIME);
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+        order.approve();
+        log.info("Order with id: {} has been approved", order.getId());
+
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        order.initCancel(failureMessages);
+        log.info("Order payment is cancelling for order with id: {}", order.getId());
+        return new OrderCancelledEvent(order, UTC_NOW_TIME);
+    }
+
+    @Override
+    public void cancelOrder(Order order) {
+        order.cancel();
+        log.info("Order with id: {} has been cancelled", order.getId());
+
+    }
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderEvent.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.order.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class OrderEvent implements DomainEvent<Order> {
+    private final Order order;
+    private final ZonedDateTime createdAt;
+}
